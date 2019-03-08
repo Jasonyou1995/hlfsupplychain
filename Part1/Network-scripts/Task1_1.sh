736e12302736f290409185ebe7ee6c26c3e35966
jasonyou1995 <jason.you1995@gmail.com>
2019-03-08 15:20:39 -0500

    change p1

--- Part1/Network-scripts/Task1_1.sh
+++ Part1/Network-scripts/Task1_1.sh
@@ -1,11 +1,6 @@
-# Author:           Jason You
-# Last modified:    March 6 2019
-# Project:          Car Components Supply Chain
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-# Root Dir: ./fabric-samples/basic-network/
-
+# Author:         Jason You
+# Last modified:  March 4 2019
+# Project:        Car Components Supply Chain
 
 
 #########################################################
@@ -13,29 +8,49 @@
 #########################################################
 
 
-# 1. generate cryptography keys with "./crypto-config.yaml"
+# What does this do?
+	# 1. generate crypto with "./crypto-config.yaml"
+# What are the linked files/parameters(s)?
+	# configuration file
+# What can I change?
+	# configuration time can be changed
 ../bin/cryptogen generate --config=./crypto-config.yaml
 
-# 2. set the currenct path to be the FABRIC_CFG_PATH
+# What does this do?
+	# 2. set the currenct path to be the FABRIC_CFG_PATH
+# What are the linked files/parameters(s)?
+	# Path of the current directory
+# What can I change?
+	# Path of the current directory
 export FABRIC_CFG_PATH=$PWD
 
 
-# ----------------------------
-# Generating peers and orderer
-# ----------------------------
+# - ---------------------------
+#   Generating peers and orderer
+# - ---------------------------
 	
-# 3. Creating the genesis block
-# ThreeOrgsOrdererGenesis is the profile defined in the configtx.yaml
-# Here we output the genesis block to the ./channel-artifacts
-# This allowing any entity communicate with the ordering service and get signature verification
+# What does this do? (creating the genesis block)
+	# 3. ThreeOrgsOrdererGenesis is the profile defined in the configtx.yaml
+	#    and here we output the genesis block to the channel-artifacts: a lot of genesis blocks
+	#    (define the consortium)
+	#    This allowing any entity communicate with the ordering service and get signature verification
+# What are the linked files/parameters(s)?
+	# the configtx.yaml
+	# `generateChannelArtifacts()` in the byfn.sh
+# What can I change?
+	# The outputBlock and the channel ID
 ../bin/configtxgen -profile ThreeOrgsOrdererGenesis -channelID byfn-sys-channel -outputBlock ./channel-artifacts/genesis.block
 
-# Defining the channel name
+# Define the channel name (not the same as the channel ID)
 export CHANNEL_NAME=mychannel
 
 
-
-# 4. Generating the channel and anchor peers artifacts for organization 1, 2, and 3 with the ThreeOrgsChannel profile
+# What does this do? (creating the channel)
+	# 4. Generating the channel and anchor peers for organization 1, 2, and 3 with the ThreeOrgsChannel profile
+# What are the linked files/parameters(s)?
+	# ./channel-artifacts/*.tx
+# What can I change?
+	# The channel and anchor peers
 ../bin/configtxgen -profile ThreeOrgsChannel -outputCreateChannelTx ./channel-artifacts/channel.tx -channelID $CHANNEL_NAME
 
 ../bin/configtxgen -profile ThreeOrgsChannel -outputAnchorPeersUpdate ./channel-artifacts/Org1MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org1MSP
--- Part1/Network-scripts/Task1_2.sh
+++ Part1/Network-scripts/Task1_2.sh
@@ -1,25 +1,33 @@
-# Author:           Jason You
-# Last modified:    March 6 2019
-# Project:          Car Components Supply Chain
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-# Root Dir: ./fabric-samples/basic-network/
+# Author:         Jason You
+# Last modified:  March 4 2019
+# Project:        Car Components Supply Chain
+
 
 #########################################################
 ####################### Task 1.2 ########################
 #########################################################
 
-# 5. docker compose configuration
-# option: docker-compose -f docker-compose-cli.yaml down
+# What does this do?
+    # 5. docker compose configuration
+# What are the linked files/parameters(s)?
+    # ./docker-compose-cli.yaml
+# What can I change?
+    # The configuration file: ./docker-compose-cli.yaml
 docker-compose -f docker-compose-cli.yaml up -d
 
-# 6. Environmental variables set up for the core peer
-# This will over write the ENV VAR we set in the configuration file
+# What does this do?
+    # 6. Environmental variables set up for the core peer
+# What are the linked files/parameters(s)?
+    # N/A
+# What can I change?
+    # Environment variables for peer0.org1.example.com:7051
 CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
 CORE_PEER_ADDRESS=peer0.org1.example.com:7051
 CORE_PEER_LOCALMSPID="Org1MSP"
 CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt
 
-# 7. Run the cli docker bash
+# What does this do?
+    # 7. Run the cli docker bash
+# What are the linked files/parameters(s)?
+# What can I change?
 docker exec -it cli bash
